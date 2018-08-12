6fae8c21cf2055de69a0c1c519671d287e35c00b
seabbs <s.e.abbott12@gmail.com>
2018-08-12 21:28:49 +0000

    added a custom state plotting function

--- R/plot_state.R
+++ R/plot_state.R
@@ -1,34 +1,35 @@
 #' Plot a Subset of States From a LiBBi Model
 #'
-#' @description Plot either the distribution or trace of a subset of parameters split out by their dimension
+#' @description Plot a subset of the states from a LiBBi model and summarising multiple runs. 
+#' Multi dimension states can be plotted using stratification and facetting.
 #' @param libbi_model  LiBBi model object
-#' @param state A character vector containing the complete names of the variables to plot.
-#' @param log_scale Logical, defaults to \code{TRUE}. Should the x axis be plotted on a log scale?
-#' @param sqrt_scale Logical, defaults to \code{TRUE}. Should the x axis be plotted on a square root scale?
-#'
+#' @param states A character vector containing the complete names of the variables to plot.
+#' @param 
 #' @return A plot of the specified states.
 #' @export
 #' @import ggplot2
-#' @import rbi bi_read
+#' @importFrom rbi bi_read
 #' @importFrom scales comma
-#' @importFrom dplyr group_by mutate group_by summarise ungroup
-#' @importFrom rbi bi_read 
-#' @importFrom purrr
+#' @importFrom dplyr group_by mutate summarise ungroup filter mutate_at
+#' @importFrom purrr map map_dfr
 #' @importFrom tibble as_tibble
+#' @importFrom tidyr gather
 #' @examples
 #' 
 #' 
-plot_state <- function(libbi_model = NULL, states = NULL, 
-                       log_scale = FALSE, sqrt_scale = FALSE,
+plot_state <- function(libbi_model = NULL, 
+                       states = NULL, 
                        summarise = FALSE,
-                       summarise_by = NULL) {
+                       summarise_by = NULL,
+                       strat_var = "state") {
   
   ## Read in data
   data <- bi_read(libbi_model)
   
   ## Filter out for required states/states
   data <- data[states] %>% 
-    map(as_tibble)
+    map(as_tibble) %>% 
+    map(~filter(., time > 0))
   
   summarise_state <- function(df, summarise = summarise, summarise_by = summarise_by) {
     if (summarise) {
@@ -37,16 +38,46 @@ plot_state <- function(libbi_model = NULL, states = NULL,
         summarise(value = sum(value, na.rm = TRUE)) %>% 
         ungroup
     }
-    
-    df <- df %>% 
-      group_by(.dots = setdiff(colnames(df), c("np", "value")))
+  ##  df <- df %>% 
+   ##   group_by(.dots = setdiff(colnames(df), c("np", "value")))
     return(df)
   }
   
+  ## Summarise by vectorisation if required.
     data <- map_dfr(data, summarise_state, 
-                summarise = TRUE, summarise_by = summarise_by, 
+                summarise = summarise, summarise_by = summarise_by, 
                 .id = "state")
+    
+    if (!(strat_var %in% colnames(data))) {
+      strat_var <- "state"
+        }
+  ## Plot traces
+    plot <- data %>% 
+      group_by(.dots = setdiff(colnames(data), c("np", "value"))) %>% 
+      summarise(
+        mean = mean(value, na.rm = TRUE),
+        median = median(value, na.rm = TRUE),
+        ll = quantile(value, 0.25, na.rm = TRUE),
+        lll = quantile(value, 0.025, na.rm = TRUE),
+        hh = quantile(value, 0.75, na.rm = TRUE),
+        hhh = quantile(value, 0.975, na.rm = TRUE)) %>% 
+      ungroup %>% 
+      gather("Average", "Count", mean, median) %>%
+      mutate_at(.vars = c(strat_var), .funs = funs(as.factor(.)))  %>% 
+      {ggplot(., aes_string(x = "time", y = "Count", linetype = "Average", col = strat_var, fill = strat_var)) +
+      geom_line(size = 1.2, alpha = 0.8) +
+      geom_ribbon(aes(ymin = lll, ymax = hhh, col = NULL), alpha = 0.1) +
+      geom_ribbon(aes(ymin = ll, ymax = hh, col = NULL), alpha = 0.2) +
+      scale_fill_viridis_d(end = 0.8) +
+      scale_color_viridis_d(end = 0.8) +
+      scale_y_continuous(labels = comma) + 
+      theme_minimal() +
+      theme(legend.position = "top") +
+      labs(x = "Time") +
+      facet_wrap(setdiff(colnames(.), c("Average", "Count", "lll", "ll", "hh", "hhh", "time", strat_var)), scales = "free_y")}
 
+    
+    return(plot)
   
 }
   
--- tests/manual-tests/test-run-model.R
+++ tests/manual-tests/test-run-model.R
@@ -6,7 +6,7 @@ tic()
 tb_model <- test_model(model= "BaseLineModel", gen_data = FALSE, run_time = 74, time_scale = "year", plot_input_data = FALSE,
                       sample_priors = TRUE, prior_samples = 1000, nparticles = NULL, adapt_particles = FALSE,
                       adapt_proposal = FALSE, min_acc = 0.05, max_acc = 0.4, fit = FALSE, posterior_samples = 100,
-                      nthreads = 4, verbose = TRUE, libbi_verbose = TRUE, browse = FALSE)
+                      nthreads = 4, verbose = TRUE, libbi_verbose = FALSE, browse = FALSE)
 
 
 toc()
