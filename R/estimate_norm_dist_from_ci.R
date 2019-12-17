ec874d2416a6d03aa67e2eff6e55e5c033008cc8
Sam Abbott <s.e.abbott12@gmail.com>
2019-12-17 13:33:46 +0000

    added ci estimator to functions

---
+++ R/estimate_norm_dist_from_ci.R
@@ -0,0 +1,43 @@
+
+#' A Function to Estimate a Normal Distribution from Credible or Confidence Intervals
+#'
+#' @param lower_interval Numeric, the lower CI.
+#' @param upper_interval Numeric, the upper CI
+#' @param interval A character string indicating the percentage interval the CI represents. Defaults to "95\%".
+#' @return A dataframe containing the mean and standard deviation of the normal distribution summarised by the provided CI's.
+#' @export
+#' @importFrom tibble tibble
+#' @examples
+#'
+#' ## Run function to estimate normal distribution for a 95% CI of 1, to 2
+#'
+#' df <- estimate_norm_dist_from_ci(1,2)
+#'
+#' ## Check
+#'
+#' x <- rnorm(10000, df$mean, df$sd)
+#'
+#' ## Mean
+#' mean(x)
+#' 
+#' ## Lower bound
+#' mean(x) + qnorm(0.025) * sd(x)
+#' 
+#' ## Upper bound
+#' mean(x) + qnorm(0.975) * sd(x)
+estimate_norm_dist_from_ci <- function(lower_interval = NULL,
+                                       upper_interval = NULL,
+                                       interval = "95%") {
+  
+  if (!(interval %in% "95%")) {
+    stop("Only a 95% credible interval has been implemented, please fork the
+       package on github and add your required interval")
+  }
+  z <- qnorm(0.975)
+  
+  mean <- lower_interval + (upper_interval - lower_interval) / 2
+  sd <- (upper_interval - lower_interval) / (2 * z)
+  
+  out <- tibble::tibble(mean = mean, sd = sd)
+  return(out)
+}
